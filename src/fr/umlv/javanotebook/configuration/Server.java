992ebc8014d83d2827e52b65ea509d5a45168989
Vahen <naeos@laposte.net>
2015-11-17 23:55:25 +0100

    Ajout du projet prototype
    Ajout du projet prototype donné par énoncé
    
    Ajout de la création du serveur dans notre projet

--- src/fr/umlv/javanotebook/configuration/Server.java
+++ src/fr/umlv/javanotebook/configuration/Server.java
@@ -1,6 +1,10 @@
 package fr.umlv.javanotebook.configuration;
 
-public class Server{
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class Server extends AbstractVerticle{
 
 	final int port; // port du server
 	final String adress; // nom du serveur , localhost car on ne travail que en local 
@@ -10,21 +14,35 @@ public class Server{
 		this.port=8989;
 	}
 	
-	// Permet de lancer le serveur
-	public void start(){
-		
-		
+	@Override
+	public void start() {
+		Router router = Router.router(vertx);
+		// route to JSON REST APIs 
+		//router.get("/exercise/:id").handler(this::getAnExercise);
+		// otherwise serve static pages
+		router.route().handler(StaticHandler.create());
+
+		vertx.createHttpServer().requestHandler(router::accept).listen(8989);
 	}
+
+//	private void getAnExercise(RoutingContext routingContext) {
+//		String id = routingContext.request().getParam("id");
+//		System.out.println("ask for an exercise by id " + id);
+//		routingContext.response()
+//		.putHeader("content-type", "application/json")
+//		.end(exercise.toJSON());
+//	}
 	
 	// Affiche l'url du serveur sur le terminal
 	public void print_url(){
-		System.out.println(this.adress+" "+this.port);
-
+		System.out.println("Copy to browser to start");
+		System.out.println(this.adress+":"+this.port);
 	}
 	
-	public boolean verify(){
-		return true;
-	}
+//	public boolean verify(){
+//		return true;
+//	}
+	
 	
 
 
--- src/fr/umlv/javanotebook/main/InterJavaBook.java
+++ src/fr/umlv/javanotebook/main/InterJavaBook.java
@@ -1,13 +1,16 @@
 package fr.umlv.javanotebook.main;
 
 import fr.umlv.javanotebook.configuration.Server;
+import io.vertx.core.Vertx;
 
 public class InterJavaBook {
 
-	
 	public static void main(String[] args) {
-		Server s = new Server();		
-		s.start();
+		Server s = new Server(); // creating server with correct parameters
+		Vertx vertx = Vertx.vertx();
+		vertx.deployVerticle(s);
 		s.print_url();
+
 	}
+
 }
