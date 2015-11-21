89155d1a62851f601a4f8baeedfe11a9cc97d58d
Vahen <naeos@laposte.net>
2015-11-21 19:17:37 +0100

    Maj libs
    Ajout des librairies manquante au projet pour le bon fonctionnement de
    Parboiled .

--- src/fr/umlv/javanotebook/exercice/Exercice.java
+++ src/fr/umlv/javanotebook/exercice/Exercice.java
@@ -7,4 +7,6 @@ public class Exercice {
 	public void print(){
 		
 	}
+	
+	
 }
---
+++ src/fr/umlv/javanotebook/exercice/ExerciceParser.java
@@ -0,0 +1,10 @@
+package fr.umlv.javanotebook.exercice;
+
+import org.parboiled.BaseParser;
+import org.parboiled.annotations.BuildParseTree;
+
+@BuildParseTree
+public class ExerciceParser extends BaseParser<Object>  {
+
+
+}
