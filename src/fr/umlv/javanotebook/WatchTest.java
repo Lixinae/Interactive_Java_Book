4b05bcfc4a6a69030044ec7ebc40304e2e9f4e16
Vahen <naeos@laposte.net>
2015-11-25 03:31:17 +0100

    Update
    Ajout des conditions de test

--- src/fr/umlv/javanotebook/WatchTest.java
+++ src/fr/umlv/javanotebook/WatchTest.java
@@ -3,6 +3,7 @@ package fr.umlv.javanotebook;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -14,48 +15,64 @@ import java.nio.file.WatchService;
 
 // Exemple de code afin voir comment fonctionne la classe "watchservice"
 public class WatchTest {
-	 
-    public static void main(String[] args) {
-        try {
-            WatchService watcher = FileSystems.getDefault().newWatchService();
-            Path dir = Paths.get("./Exercice/");
-            dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-             
-            System.out.println("Watch Service registered for dir: " + dir.getFileName());
-             
-            while (true) {
-                WatchKey key;
-                try {
-                    key = watcher.take();
-                } catch (InterruptedException ex) {
-                    return;
-                }
-                 
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    WatchEvent.Kind<?> kind = event.kind();
-                     
-                    @SuppressWarnings("unchecked")
-                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    Path fileName = ev.context();
-                    
-                    // Peut ajouter des test a ce moment pour voir ce que l'on fait en cas de modification
-                    
-                    System.out.println(kind.name() + ": " + fileName);
-                     
-                    if (kind == ENTRY_MODIFY &&
-                        fileName.toString().equals("WatchTest.java")) {
-                        System.out.println("My source file has changed!!!");
-                    }
-                }
-                 
-                boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
-            }
-             
-        } catch (IOException ex) {
-            System.err.println(ex);
-        }
-    }
+
+	public static void main(String[] args) {
+		try {
+			WatchService watcher = FileSystems.getDefault().newWatchService();
+			Path dir = Paths.get("./Exercice/");
+			dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+			System.out.println("Watch Service registered for dir: " + dir.getFileName());
+
+			while (true) {
+				WatchKey key;
+				try {
+					key = watcher.take();
+				} catch (InterruptedException ex) {
+					return;
+				}
+
+				for (WatchEvent<?> event : key.pollEvents()) {
+					WatchEvent.Kind<?> kind = event.kind();
+
+					@SuppressWarnings("unchecked")
+					WatchEvent<Path> ev = (WatchEvent<Path>) event;
+					Path fileName = ev.context();
+
+					// Peut ajouter des test a ce moment pour voir ce que l'on fait en cas de modification
+
+					System.out.println(kind.name() + ": " + fileName);
+
+					if (kind == OVERFLOW) {
+						continue;
+					} else if (kind == ENTRY_CREATE) {
+
+						// process create event
+
+					} else if (kind == ENTRY_DELETE) {
+
+						// process delete event
+
+					} else if (kind == ENTRY_MODIFY) {
+
+						// process modify event
+
+					}
+
+					if (kind == ENTRY_MODIFY &&
+							fileName.toString().equals("WatchTest.java")) {
+						System.out.println("My source file has changed!!!");
+					}
+				}
+
+				boolean valid = key.reset();
+				if (!valid) {
+					break;
+				}
+			}
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
 }
