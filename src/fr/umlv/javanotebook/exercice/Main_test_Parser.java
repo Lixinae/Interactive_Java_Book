e3c3bca7563e45114ff1044b987dae9baf1751ad
PICO CHRISTOPHER <cpico@localhost.localdomain>
2015-11-26 15:27:13 +0100

    Maj Parser -> fini

--- src/fr/umlv/javanotebook/exercice/Main_test_Parser.java
+++ src/fr/umlv/javanotebook/exercice/Main_test_Parser.java
@@ -12,62 +12,50 @@ import java.util.List;
 
 public class Main_test_Parser {
 
-	private static String getTextContent(Node node) {
-		if (node instanceof TextNode) {
-			return getTextContent((TextNode)node);
-		} else if (node instanceof HeaderNode) {
-			HeaderNode headerNode = (HeaderNode) node;
-			return getTextContent((TextNode) headerNode.getChildren().get(0));
-		} else if (node instanceof ParaNode) {
-			ParaNode paraNode = (ParaNode) node;
-			Node firstChildNode = paraNode.getChildren().get(0);
-			if (firstChildNode instanceof SuperNode) {
-				return getTextContent((SuperNode) firstChildNode);
-			} else if (firstChildNode instanceof TextNode) {
-				return getTextContent((TextNode) firstChildNode);
-			}
-		} else if (node instanceof ListItemNode) {
-			ListItemNode listItemNode = (ListItemNode) node;
-			RootNode rootNode = (RootNode) listItemNode.getChildren().get(0);
-			Node firstChildNode = rootNode.getChildren().get(0);
-			if (firstChildNode instanceof SuperNode) {
-				return getTextContent((SuperNode) firstChildNode);
-			} else if (firstChildNode instanceof TextNode) {
-				return getTextContent((TextNode) firstChildNode);
-			}
-		}
-		return null;
-	}
+//	private static String getTextContent(Node node) {
+//		if (node instanceof TextNode) {
+//			return getTextContent((TextNode)node);
+//		} else if (node instanceof HeaderNode) {
+//			HeaderNode headerNode = (HeaderNode) node;
+//			return getTextContent((TextNode) headerNode.getChildren().get(0));
+//		} else if (node instanceof ParaNode) {
+//			ParaNode paraNode = (ParaNode) node;
+//			Node firstChildNode = paraNode.getChildren().get(0);
+//			if (firstChildNode instanceof SuperNode) {
+//				return getTextContent((SuperNode) firstChildNode);
+//			} else if (firstChildNode instanceof TextNode) {
+//				return getTextContent((TextNode) firstChildNode);
+//			}
+//		} else if (node instanceof ListItemNode) {
+//			ListItemNode listItemNode = (ListItemNode) node;
+//			RootNode rootNode = (RootNode) listItemNode.getChildren().get(0);
+//			Node firstChildNode = rootNode.getChildren().get(0);
+//			if (firstChildNode instanceof SuperNode) {
+//				return getTextContent((SuperNode) firstChildNode);
+//			} else if (firstChildNode instanceof TextNode) {
+//				return getTextContent((TextNode) firstChildNode);
+//			}
+//		}
+//		return null;
+//	}
+//
+//	private static String getTextContent(SuperNode node) {
+//		List<Node> nodes = node.getChildren();
+//		StringBuilder content = new StringBuilder();
+//		for (Node child : nodes) {
+//			if (child instanceof TextNode) {
+//				content.append(getTextContent((TextNode)child));
+//			} else if (child instanceof SpecialTextNode) {
+//				content.append(getTextContent((SpecialTextNode)child));
+//			}
+//		}
+//		return content.toString();
+//	}
+//
+//	private static String getTextContent(TextNode node) {
+//		return node.getText();
+//	}
 
-	private static String getTextContent(SuperNode node) {
-		List<Node> nodes = node.getChildren();
-		StringBuilder content = new StringBuilder();
-		for (Node child : nodes) {
-			if (child instanceof TextNode) {
-				content.append(getTextContent((TextNode)child));
-			} else if (child instanceof SpecialTextNode) {
-				content.append(getTextContent((SpecialTextNode)child));
-			}
-		}
-		return content.toString();
-	}
-
-	private static String getTextContent(TextNode node) {
-		return node.getText();
-	}
-
-
-	/* TODO
-	 * Help 
-	 * 
-	 * Liste des noms de noeud possible:
-	 * 
-	 * 		ParaNode : paragraphe -> <p></p> en html
-	 * 		BulletListNode : Liste d'element avec * devant . Liste en html <li><ul></ul></li>
-	 * 		HeaderNode : header <h1></h1> , h2 etc....
-	 * 		VerbatimNode : <code></code>
-	 * 
-	 */
 
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -77,22 +65,27 @@ public class Main_test_Parser {
 		Preconditions.checkNotNull(markdown, "The specified file isn't found - "+fichier);
 		PegDownProcessor processor = new PegDownProcessor(Extensions.ALL);
 
-		RootNode rootNode = processor.parseMarkdown(markdown);
-
-		// recupère la liste des noeud
-		List<Node> nodes = rootNode.getChildren();
-
-		// Affiche ce qu'il y a chaque noeud
-		for ( Node node : nodes){
-			if (node instanceof HeaderNode) {
-				HeaderNode headerNode = (HeaderNode) node;
-				String text = getTextContent(node);
-				System.out.println(text);
-			}
-			else if(node instanceof HeaderNode){
-
-			}
-			System.out.println(node);
-		}
+		// convertie le fichier markdown au format html
+		// envoyer la string au server qui l'a renvoie au client
+		String html_code=processor.markdownToHtml(markdown);
+		
+		System.out.println(html_code);
+//		RootNode rootNode = processor.parseMarkdown(markdown);
+//
+//		// recupï¿½re la liste des noeud
+//		List<Node> nodes = rootNode.getChildren();
+//
+//		// Affiche ce qu'il y a chaque noeud
+//		for ( Node node : nodes){
+//			if (node instanceof HeaderNode) {
+//				HeaderNode headerNode = (HeaderNode) node;
+//				String text = getTextContent(node);
+//				System.out.println(text);
+//			}
+//			else if(node instanceof HeaderNode){
+//
+//			}
+//			System.out.println(node);
+//		}
 	}
 }
