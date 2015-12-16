fccc14d8b0caaab5e2d6786a407c6b48e2bb9049
Vahen <naeos@laposte.net>
2015-12-16 19:15:39 +0100

    Ajout d'un fichier de reponse
    Ajout d'un fichier de reponse et du parser correspondant.

--- src/fr/umlv/javanotebook/exercice/Exercices.java
+++ src/fr/umlv/javanotebook/exercice/Exercices.java
@@ -1,43 +1,50 @@
 package fr.umlv.javanotebook.exercice;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Objects;
 
 public class Exercices {
 
-//	private final int id;
-//	private String respons;
-	
-	private final HashMap<Integer,Object> exercices = new HashMap<>();
+	//	private final int id;
+	//	private String respons;
+
+	private final HashMap<Integer,String> exercices ;
+
+	public Exercices(){
+		exercices = new HashMap<>();
+	}
 	
 	/**
 	 * Fetches all the exercise
 	 * Associating the id of the exercise with the awaited respons
 	 */
-	public void getAllExercices(){
-		// for
-		
-		//exercices.put(key, value);
+	public void getAllExercicesAndAnswers(){
+		Path path = Paths.get("./exercice/answers.rep");
+		try {
+			Files.lines(path).forEach(l->{
+				exercices.put(Integer.parseInt(l.split(" : ")[0]),l.split(" : ")[1]);
+			});
+		} catch (IOException e) {
+			System.err.println(e);
+		}
 	}
 
-	/**
-	 * Changes the answer of an exercise
-	 * 
-	 * @param id : id of the exercice
-	 * @param newAnswer : new Answer
-	 */
-	public void updateAnswer(int id,Object newAnswer){
-		Objects.requireNonNull(newAnswer);
-		exercices.put(id, newAnswer);
-	}
-	
 	/**
 	 * 
 	 * @return returns the number of exercises in the exercice folder
 	 */
 	public static String countFiles(){
-		return ""+new File("./exercice").listFiles().length;
+		return ""+(new File("./exercice").listFiles().length-1);
+	}
+
+	public static void main(String[] args) {
+		Exercices ex = new Exercices() ;
+		ex.getAllExercicesAndAnswers();
+		System.out.println(ex.exercices);
 	}
 	
 }
