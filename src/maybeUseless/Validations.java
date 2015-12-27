d8719ee7d105b1281971442aba2ed4599d4fa91a
Vahen <naeos@laposte.net>
2015-12-27 17:30:23 +0100

    Correctifs

---
+++ src/maybeUseless/Validations.java
@@ -0,0 +1,20 @@
+//package fr.umlv.javanotebook.validation;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Validations {
+//	private final List<Validation> validations = new ArrayList<>();
+//	public Validations() {
+//
+//	}
+//	public boolean add(Validation val){
+//		return validations.add(val);
+//	}
+//	public int length() {
+//		return validations.size();
+//	}
+//	public Validation get(int index) {
+//		return validations.get(index);
+//	}
+//}
